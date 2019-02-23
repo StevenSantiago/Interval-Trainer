e50110ea89e23a6b57ccfe6a0a42233b2203f837
Steven <stevenalfred93@yahoo.com>
2019-02-22 23:53:19 -0500

    Monospace digits for custom font does not seem to work. Will need to chose different font that supports monospace or tinker with the ttf file to adjust and make monospace.

--- Interval Trainer/DisplayTimer.swift
+++ Interval Trainer/DisplayTimer.swift
@@ -8,6 +8,58 @@
 
 import UIKit
 
+@IBDesignable
 class DisplayTimer: UILabel {
+
     
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpLabel()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpLabel()
+    }
+    
+    func setUpLabel(){
+        
+        let originalFont = UIFont(name: "Laserian", size: 45)
+        var originalFontDescriptor = originalFont!.fontDescriptor
+        //print(originalFontDescriptor)
+
+        let fontDescriptorFeatureSettings = [
+            [
+                UIFontDescriptor.FeatureKey.featureIdentifier: kNumberSpacingType,
+                UIFontDescriptor.FeatureKey.typeIdentifier: kMonospacedNumbersSelector
+            ]
+        ]
+
+        let fontDescriptorFeatureSettingsT = [
+            [
+                UIFontDescriptor.FeatureKey.featureIdentifier:kAllTypographicFeaturesType,
+                UIFontDescriptor.FeatureKey.typeIdentifier: kAllTypeFeaturesOnSelector
+            ]
+        ]
+
+        let fontDescriptorAttributes = [UIFontDescriptor.AttributeName.featureSettings: fontDescriptorFeatureSettings]
+        originalFontDescriptor = originalFontDescriptor.addingAttributes(fontDescriptorAttributes)
+        originalFontDescriptor = originalFontDescriptor.addingAttributes([UIFontDescriptor.AttributeName.featureSettings: fontDescriptorFeatureSettingsT])
+        let fontDescriptor = originalFontDescriptor
+
+        //print(fontDescriptor)
+
+        let font = UIFont(descriptor: fontDescriptor, size: 0.0)
+
+        self.font = font
+
+
+        
+//        let attributedString = NSMutableAttributedString(string: "111111111")
+//        attributedString.addAttribute(NSAttributedString.Key.kern, value: CGFloat(1.5), range: NSRange(location: 0, length: 9))
+//
+//        self.attributedText = attributedString
+        self.adjustsFontSizeToFitWidth = true
+    }
 }
