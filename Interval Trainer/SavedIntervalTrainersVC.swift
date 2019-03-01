d78874610a06155f8623584251eca7dc1d0ff962
Steven <stevenalfred93@yahoo.com>
2019-02-28 23:31:40 -0500

    Added code to pull dummy data to display on table view.  This data will need to come from Core Data

--- Interval Trainer/SavedIntervalTrainersVC.swift
+++ Interval Trainer/SavedIntervalTrainersVC.swift
@@ -12,20 +12,39 @@ class SavedIntervalTrainersVC: UIViewController, UITableViewDataSource, UITableV
 
     @IBOutlet weak var savedTimers: UITableView!
     
+    var timers:[Timers] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController!.navigationBar.isHidden = false
         savedTimers.dataSource = self
         savedTimers.delegate = self
+        
+        timers = createTimers()
+    }
+    
+    func createTimers() -> [Timers] {
+        var tempTimers:[Timers] = []
+        
+        let timer1 = Timers(hour: 0, minute: 1, second: 10, restTime: 45, activeTime: 50, currentRunTime: 0, sets: 10)
+        
+        let timer2 = Timers(hour: 0, minute: 0, second: 30, restTime: 10, activeTime: 30, currentRunTime: 0, sets: 15)
+        
+        tempTimers.append(timer1)
+        tempTimers.append(timer2)
+        
+        return tempTimers
     }
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return timers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TimerCell", for: indexPath) as! SavedTimer
+        let timer = timers[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimerCell", for: indexPath) as! TimerCell
+        cell.setTimerCell(timer: timer)
         return cell
     }
 
---
+++ Interval Trainer/TimerCell.swift
@@ -0,0 +1,27 @@
+//
+//  SavedTimer.swift
+//  Interval Trainer
+//
+//  Created by Steven on 2/27/19.
+//  Copyright © 2019 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class TimerCell: UITableViewCell {
+    
+    @IBOutlet weak var InteralTimerName: UILabel!
+    @IBOutlet weak var sets: UILabel!
+    @IBOutlet weak var activeTime: UILabel!
+    @IBOutlet weak var restTime: UILabel!
+    
+    
+    func setTimerCell(timer: Timers){
+        InteralTimerName.text = "Example Timer"
+        self.sets.text = String(timer.sets)
+        self.activeTime.text = String(timer.activeTime)
+        self.restTime.text = String(timer.restTime)
+    }
+}
+
+
