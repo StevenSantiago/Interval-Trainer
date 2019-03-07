2dafbc3ca183c47bc847358d0d8c246da06ed1e9
Steven <stevenalfred93@yahoo.com>
2019-03-06 22:56:22 -0500

    Removed unused and commented code and print statements

--- Interval Trainer/SavedIntervalTrainersVC.swift
+++ Interval Trainer/SavedIntervalTrainersVC.swift
@@ -13,7 +13,6 @@ class SavedIntervalTrainersVC: UIViewController, UITableViewDataSource, UITableV
 
     @IBOutlet weak var savedTimers: UITableView!
     
-    var timers:[Timers] = []
     var intervalTimers:[IntervalTimer] = []
     
     override func viewDidLoad() {
@@ -22,7 +21,6 @@ class SavedIntervalTrainersVC: UIViewController, UITableViewDataSource, UITableV
         savedTimers.dataSource = self
         savedTimers.delegate = self
         
-        //timers = createTimers()
         
         let fetchRequest: NSFetchRequest<IntervalTimer> = IntervalTimer.fetchRequest()
         
@@ -41,21 +39,6 @@ class SavedIntervalTrainersVC: UIViewController, UITableViewDataSource, UITableV
     }
     
     
-    
-    //This is for testing purposes. Creating will come from database store call from CreateIntervalTimer VC
-//    func createTimers() -> [Timers] {
-//        var tempTimers:[Timers] = []
-//
-//        let timer1 = Timers(name:"Jump Rope", hour: 0, minute: 1, second: 10, restTime: 45, activeTime: 70, currentRunTime: 70, sets: 10)
-//
-//        let timer2 = Timers(name:"Body Weight", hour: 0, minute: 0, second: 30, restTime: 10, activeTime: 30, currentRunTime: 30, sets: 15)
-//
-//        tempTimers.append(timer1)
-//        tempTimers.append(timer2)
-//
-//        return tempTimers
-//    }
-    
     func removeIntervalTimer(indexPath: IndexPath){
         guard let managedContext = appDelegate?.persistentContainer.viewContext else{return}
         managedContext.delete(intervalTimers[indexPath.row])
@@ -73,12 +56,9 @@ class SavedIntervalTrainersVC: UIViewController, UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
-            print("Number of intervalTimers is: \(self.intervalTimers.count)")
             self.removeIntervalTimer(indexPath: indexPath)
             self.fetchCoreData()
-            print("Number of intervalTimers is: \(self.intervalTimers.count)")
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            
         }
         deleteAction.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
         return [deleteAction]
